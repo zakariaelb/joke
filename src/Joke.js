949b1256bc5cec8a2b6622e19682ed76a0d0d69c
DIGIPlus <admin@digiplus.cf>
2023-04-24 21:26:54 +0400

    fin

--- src/Joke.js
+++ src/Joke.js
@@ -13,8 +13,9 @@ export default function Joke(props) {
         <div>
             {props.setup && <h3>{props.setup}</h3>}
             {isShown && <p>{props.punchline}</p>}
-            {isShown && <button onClick={toggleShown}>Hide Punchline</button>}
-            {!isShown && <button onClick={toggleShown}>Show Punchline</button>}
+            {/* {isShown && <button onClick={toggleShown}>Hide Punchline</button>}
+            {!isShown && <button onClick={toggleShown}>Show Punchline</button>} */}
+            <button onClick={toggleShown}> {isShown ? "Hide": "Show"} Punchline</button>
             <hr />
         </div>
     )
